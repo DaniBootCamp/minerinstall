5b224d525ce8355f200ccba180853e218f682451
DaniBootCamp <40635516+DaniBootCamp@users.noreply.github.com>
2024-01-17 19:06:30 +0100

    Delete dotnet.sh

--- dotnet.sh
+++
@@ -1,9 +0,0 @@
-# First, try to remove/uninstall older .Net, if any, then install .Net 6
-echo "$(tput setaf 3)Installing .Net 6$(tput sgr0)"
-sudo apt remove 'dotnet*'
-sudo apt remove 'aspnetcore*'
-sudo apt remove 'netstandard*'
-sudo rm /etc/apt/sources.list.d/microsoft-prod.list
-sudo rm /etc/apt/sources.list.d/microsoft-prod.list.save
-sudo apt update
-sudo apt install dotnet6
