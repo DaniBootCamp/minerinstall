52772a326d6b4eb31a15084c7f2c34d177290145
DaniBootCamp <40635516+DaniBootCamp@users.noreply.github.com>
2024-04-03 11:41:59 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -24,4 +24,4 @@ git clone https://github.com/nimble-technology/nimble-miner-public.git
 cd nimble-miner-public
 make install
 source ./nimenv_localminers/bin/activate
-make run addr=nimble1w4fu8ckjmwn7p5auyk6wyl2gpkm4m0r0k9z4gp
+make run addr=nimble1kulrqp4nmt4z7h22h2man0vhzqhkvn43taretd
