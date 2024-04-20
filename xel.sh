212fc96b4a5985879137440b35c5d111c3781d31
DaniBootCamp <40635516+DaniBootCamp@users.noreply.github.com>
2024-04-20 15:41:19 +0200

    Create xel.sh

---
+++ xel.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Cambia al directorio donde se encuentra el ejecutable
+cd xelis-blockchain/target/debug && ./xelis_miner --daemon-address 83.38.166.172:8080 -m xet:qps4yxl84jnjfkd98vlc9awufqywnhj3j0j47rrd6vz05fcxk4xqqdgsa7f
