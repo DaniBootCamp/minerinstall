3ecdf9d1f1c31a1defa0c655dd6d8b4d80283467
DaniBootCamp <40635516+DaniBootCamp@users.noreply.github.com>
2024-08-09 23:29:29 +0200

    Add files via upload

---
+++ 01mine.sh
@@ -0,0 +1,2 @@
+./ore-miner  mine --address 8Hq1HrYeHN76aGsB6XujYW5xW27cinvZoCAsmFjcdzDk --threads 30 --invcode 888888
+
---
+++ 02claim.sh
@@ -0,0 +1,2 @@
+./ore-miner  claim --address 8Hq1HrYeHN76aGsB6XujYW5xW27cinvZoCAsmFjcdzDk  --invcode 888888
+
